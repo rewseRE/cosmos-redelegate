eb77785aa244ec7db26028b90cbb61dd21b88222
rewse <36141290+rewseRE@users.noreply.github.com>
2025-07-09 21:11:19 +0300

    Update redelegate.sh

--- redelegate.sh
+++ redelegate.sh
@@ -3,10 +3,11 @@
 source $(dirname $(readlink -e $0))/config
 
 #REWARDS=$($CLI query distribution rewards $ADDR $OPER --node $NODE --output json | jq -r ".rewards[] | select (.denom == (\"$DENOM\").amount")
-REWARDS=$($CLI query distribution rewards $ADDR --node $NODE --output json | jq -r ".rewards[] | select (.validator_address == \"$OPER\").reward[] | select (contains (\"$DENOM\"))" | cut -d'.' -f1)
+#REWARDS=$($CLI query distribution rewards $ADDR --node $NODE --output json | jq -r ".rewards[] | select (.validator_address == \"$OPER\").reward[] | select (contains (\"$DENOM\"))" | cut -d'.' -f1)
+REWARDS=$($CLI query distribution rewards $ADDR --node $NODE --output json | jq -r ".rewards[] | select (.validator_address == \"$OPER\").reward[] | select (.denom | index (\"$DENOM\")).amount" | cut -d'.' -f1)
 #COMMISSION=$($CLI query distribution commission $OPER --node $NODE --output json | jq -r ".commission[] | select (.denom == \"$DENOM\").amount")
-COMMISSION=$($CLI query distribution commission $OPER --node $NODE --output json | jq -r ".commission.commission[] | select (contains(\"$DENOM\"))" | cut -d'.' -f1)
-
+#COMMISSION=$($CLI query distribution commission $OPER --node $NODE --output json | jq -r ".commission.commission[] | select (contains(\"$DENOM\"))" | cut -d'.' -f1)
+COMMISSION=$($CLI query distribution commission $OPER --node $NODE --output json | jq -r ".commission.commission[] | select (.denom | index(\"$DENOM\")).amount" | cut -d'.' -f1)
 
 AMOUNT=$((${REWARDS%.*}+${COMMISSION%.*}))
 
